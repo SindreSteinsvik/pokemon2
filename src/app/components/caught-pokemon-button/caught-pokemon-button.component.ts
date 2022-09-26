bf1360ded668157cc63bb5279f5ac7d5bebeae44
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-09-26 17:07:40 +0200

    fix: icon now updates correctly for the pokemon catalogue when added to the trainer

--- src/app/components/caught-pokemon-button/caught-pokemon-button.component.ts
+++ src/app/components/caught-pokemon-button/caught-pokemon-button.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Trainer } from 'src/app/models/trainer.model';
 import { CaughtService } from 'src/app/services/caught.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-caught-pokemon-button',
@@ -10,6 +11,9 @@ import { CaughtService } from 'src/app/services/caught.service';
 })
 export class CaughtPokemonButtonComponent implements OnInit {
 
+  public hasCaught: boolean = false;
+  
+
   @Input() pokemonName: string ="";
 
   get loading(): boolean {
@@ -17,10 +21,12 @@ export class CaughtPokemonButtonComponent implements OnInit {
   }
 
   constructor(
+    private readonly trainerService: TrainerService,
     private readonly caughtService: CaughtService
   ) { }
 
   ngOnInit(): void {
+    this.hasCaught = this.trainerService.hasCaught(this.pokemonName);
   }
 
   onFavoriteClick(): void {
@@ -28,12 +34,11 @@ export class CaughtPokemonButtonComponent implements OnInit {
     this.caughtService.addToCaught(this.pokemonName)
     .subscribe({
       next: (response: Trainer) => {
-        console.log("NEXT" + response)
         alert("Caught a Pokemon: " + this.pokemonName)
+        this.hasCaught = this.trainerService.hasCaught(this.pokemonName)
       },
       error: (error: HttpErrorResponse) => {
         console.log("ERROR" + error.message)
-        alert("ERROR" + error.message)
       }
     })
   }

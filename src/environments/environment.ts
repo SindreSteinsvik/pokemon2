b328b464c52a0f7200705ac8d6951d3fce2c86ed
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-09-26 13:58:01 +0200

    fix: add picture link to non prduction state as well

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: false,
   apiTrainers: "https://omgekb-noroff-assignment4.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon/",
+  apiPictures: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
   apiKey: "p0g4FRX8Z0ylxve1Iaf5Xw==",
 };
 

35e2fc362437fe0a23d9c5468d1184b4ef0a14ba
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-09-27 16:03:20 +0200

    fix: hide apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiTrainers: "https://omgekb-noroff-assignment4.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151",
   apiPictures: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
-  apiKey: "p0g4FRX8Z0ylxve1Iaf5Xw==",
+  apiKey: "p0g5FRX8Z0ylxve1Iaf5Xw==",
 };

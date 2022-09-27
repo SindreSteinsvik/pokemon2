0098f780d12144ff7e63ecc0eba50dae7cbd635a
SindreSteinsvik <71326914+SindreSteinsvik@users.noreply.github.com>
2022-09-27 16:51:13 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiTrainers: "https://omgekb-noroff-assignment4.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151",
   apiPictures: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/",
-  apiKey: "",
+  apiKey: "p0g5FRX8Z0ylxve1Iaf5Xw==",
 };
